4f979af658571e2729966875d5e5445bd674183e
CarolinaHV <carolina.hv02@gmail.com>
2020-11-21 21:29:53 +0000

    firts commit

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_BASIC_H
+#define SHELL_BASIC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+
+
+#endif
